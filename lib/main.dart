8c344d2716562676f0ba01ff3d787b0a7ba6ead7
Dandy Zain <83494409+dandyrahmatzain21@users.noreply.github.com>
2022-09-13 22:18:28 +0700

    Ditambah Validator Dan Snackbar

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
-    //replace here
+    //replace here/
         twilioPhoneVerify = TwilioPhoneVerify(
         accountSid: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
         serviceSid: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
